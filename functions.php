f47f2de59ad4bbb1337d5b39ed4bfb413f28d84e
grigiriy <grigiriy.malyshev@gmail.com>
2022-06-11 17:25:01 +0300

    remove duplicate function

--- functions.php
+++ functions.php
@@ -84,10 +84,3 @@ function crb_register_custom_fields()
 {
   include_once __DIR__ . '/theme-helpers/custom-fields/base.php';
 }
-
-add_action('after_setup_theme', 'crb_load');
-function crb_load()
-{
-  require_once('vendor/autoload.php');
-  \Carbon_Fields\Carbon_Fields::boot();
-}
